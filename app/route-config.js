47487e5eb05d2b40bd584adf0943fcc4e631bb85
Mick <bellerophons.fail@gmail.com>
2022-02-15 18:16:03 +0300

    added route filter

--- app/route-config.js
+++ app/route-config.js
@@ -3,15 +3,22 @@
 angular.module('eStore').
 	config(['$locationProvider','$routeProvider',function config($locationProvider,$routeProvider) {
 		$locationProvider.hashPrefix('!');
+		function route(url) {
+			if(location.hostname=='localhost') {
+				return url
+			} else {
+				return '/eStore/app/' + url
+			}
+		}
 		$routeProvider.
 			when('/checkout', {
-				templateUrl:'/checkout/checkout.template.html'
+				templateUrl:route('/checkout/checkout.template.html')
 			}).
 			when('/complete', {
-				templateUrl:'/checkout/thanks.template.html'
+				templateUrl:route('/checkout/thanks.template.html')
 			}).
 			when('/placeorder', {
-				templateUrl:'/checkout/placeOrder.template.html'
+				templateUrl:route('/checkout/placeOrder.template.html')
 			}).
 			when('/products', {
 				templateUrl:'productList.html'
